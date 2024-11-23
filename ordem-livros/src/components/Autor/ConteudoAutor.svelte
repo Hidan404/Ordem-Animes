9a293c5a749cbd7f8b442d83cddf6b14bb66ee2c
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2024-11-22 21:58:10 -0300

    update pages

---
+++ ordem-livros/src/components/Autor/ConteudoAutor.svelte
@@ -0,0 +1,8 @@
+<script>
+   export let autor = null
+</script>
+
+{#if autor}
+<div>
+    <img src={autor.imagem} alt="Foto do autor">
+</div>
--- ordem-livros/src/routes/autores/george-martin/+page.svelte
+++ ordem-livros/src/routes/autores/george-martin/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+    import conteudoDoAutor from "../../../components/Autor/ConteudoAutor.svelte"
+</script>
+
 <h1 class="text-3xl font-bold">George Martin</h1>
 <a href="/">Voltar</a>
